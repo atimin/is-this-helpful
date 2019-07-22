614a364fc36b7fb433b1a0d1d11b2803d3dd73b3
Aleksey Timin <atimin@gmail.com>
2019-07-22 17:59:37 +0500

    Add helpful and not helpful buttons.

--- templates/docsite/ith_widget.js
+++ templates/docsite/ith_widget.js
@@ -1,10 +1,11 @@
 function ituWidget() {
-    function sendAction(actionName) {
+    function sendAction(actionName, data="") {
       let domainName = window.location.hostname;
       let path = window.location.pathname;
       let payload = {
-        path:  path,
-        action: actionName,
+          path:  path,
+          action: actionName,
+          data: data
       }
 
       $.ajax({
@@ -12,12 +13,34 @@ function ituWidget() {
           method: 'POST',
           dataType: 'json',
           data: JSON.stringify(payload),
-      }).done(function () {
-          alert('Hi!')
       });
 
     };
 
     sendAction('VIEW');
+    $('#itu-widget').empty()
+        .append('<p>Is this document helpful? <a id="is-helpful" href="">Yes</a> |<a id="is-not-helpful" href="">Not</a></p>');
+
+    $('#is-helpful').click(function (event) {
+        event.preventDefault();
+        sendAction('HELPFUL');
+        $('#itu-widget').empty().append('<p>Thank you for the feedback. We’re really glad we could help!</p>')
+    });
+
+    $('#is-not-helpful').click(function (event) {
+        event.preventDefault();
+        $('#itu-widget').empty()
+            .append('<p>Thank you for the feedback. How could we improve this document?</p>')
+            .append('<div><textarea name="content"  cols="70" rows="5" class="textarea" id="not-helpful-comment"/></div>')
+            .append('<input id="not-helpful-submit" type="submit" value="Submit"/>');
+
+        $('#not-helpful-submit').click(function (event) {
+            event.preventDefault();
+            sendAction('NOT_HELPFUL', $('#not-helpful-comment').val());
+            $('#itu-widget').empty()
+                .append('<p>Thank you for the feedback. Your comments will help us improve our documents in the future.</p>')
+
+        })
+    });
 }
 
