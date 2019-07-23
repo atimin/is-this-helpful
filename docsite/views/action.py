078c65b255c9c1ac05f5f21ace708f9e2a665b05
Aleksey Timin <atimin@gmail.com>
2019-07-23 14:08:00 +0500

    handle CORS in OPTIONS method

--- docsite/views/action.py
+++ docsite/views/action.py
@@ -19,21 +19,27 @@ LOGGER = logging.getLogger(__file__)
 
 @csrf_exempt
 def post_action(request, domain_name):
-    if request.method != 'POST':
-        return HttpResponse(status=405)
+    if request.method == 'POST':
 
-    payload = json.loads(request.body)
-    if not ACTION_PAYLOAD_SCHEMA.is_valid(payload):
-        return HttpResponse(status=422)
+        payload = json.loads(request.body)
+        if not ACTION_PAYLOAD_SCHEMA.is_valid(payload):
+            return HttpResponse(status=422)
 
-    site = Site.objects.filter(domain_name=domain_name).first()
+        site = Site.objects.filter(domain_name=domain_name).first()
 
-    if site:
-        doc, _ = site.document_set.get_or_create(path=payload['path'])
-        action = Action(document_id=doc.id, action=payload['action'])
-        action.data = payload['data'] if 'data' in payload else None
-        action.save()
-        return HttpResponse()
-    else:
-        return HttpResponse(status=404)
+        if site:
+            doc, _ = site.document_set.get_or_create(path=payload['path'])
+            action = Action(document_id=doc.id, action=payload['action'])
+            action.data = payload['data'] if 'data' in payload else None
+            action.save()
+            return HttpResponse()
+        else:
+            return HttpResponse(status=404)
+    elif request.method == 'OPTIONS':
+        response = HttpResponse(status=200)
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Methods"] = "POST, OPTIONS"
+        return response
+
+    return HttpResponse(status=405)
 
