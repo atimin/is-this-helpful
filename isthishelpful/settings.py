0004ab404eeeb0d9bcab5c68f340c30944977603
Aleksey Timin <atimin@gmail.com>
2019-07-23 10:26:06 +0500

    change security settings

--- isthishelpful/settings.py
+++ isthishelpful/settings.py
@@ -23,14 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bvxrl7&=wu%#5-0=+!w%q^ga-!zhnu-90knu(_#noo2=@9zbp6'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'bvxrl7&=wu%#5-0=+!w%q^ga-!zhnu-90knu(_#noo2=@9zbp6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = [
-    '0.0.0.0',
-    '127.0.0.1',
     'is-this-helpful.herokuapp.com'
 ]
 
