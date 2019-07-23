546dc77252959ace4994beadc0c67a3c6f52b6a3
Aleksey Timin <atimin@gmail.com>
2019-07-23 09:55:40 +0500

    Add site statisctics.

--- docsite/urls.py
+++ docsite/urls.py
@@ -1,9 +1,10 @@
-from docsite.views import DocsiteView, SiteCreate, post_action, WidgetScripView
+from docsite.views import DocsiteView, SiteCreate, post_action, WidgetScripView, SiteDetailView
 from django.urls import path
 
 urlpatterns = [
     path('', DocsiteView.as_view(), name='index'),
     path('sites/create', SiteCreate.as_view(), name='create_site'),
+    path('sites/<str:domain_name>/show', SiteDetailView.as_view(), name='view_site'),
     path('sites/<str:domain_name>/action/new', post_action, name='post_action'),
     path('ith_widget.js', WidgetScripView.as_view(), name='ith_widget')
 ]
--- docsite/views/site.py
+++ docsite/views/site.py
@@ -1,9 +1,20 @@
 from django.views.generic.edit import CreateView
-from docsite.models import Site
+from django.views.generic.base import TemplateView, ContextMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.timezone import datetime, timedelta
 from django.urls import reverse_lazy
+from docsite.models import Site,  Action
 
 
-class SiteCreate(CreateView):
+class SitesLoadedMixin(ContextMixin):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sites'] = Site.objects.filter(owner=self.request.user.id)
+        return context
+
+
+class SiteCreate(LoginRequiredMixin, SitesLoadedMixin, CreateView):
     model = Site
     fields = ['domain_name']
     success_url = reverse_lazy('index')
@@ -12,8 +23,26 @@ class SiteCreate(CreateView):
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['sites'] = Site.objects.filter(owner=self.request.user.id)
-        return context
 
+class SiteDetailView(LoginRequiredMixin, SitesLoadedMixin, TemplateView):
+    template_name = 'docsite/site_detail.html'
+
+    def get_context_data(self, domain_name, **kwargs):
+        context = super().get_context_data(**kwargs)
+        site = Site.objects.filter(domain_name=domain_name).first()
+        if site:
+            docs = []
+            for doc in site.document_set.all():
+                last_14_days_actions = Action.objects.filter(document_id=doc.id,
+                                                             timestamp__lte=datetime.now(),
+                                                             timestamp__gte=(datetime.now() - timedelta(14))).all()
+                doc.viewed_count = len([a for a in last_14_days_actions if a.action == 'VIEW'])
+                doc.helpful_count = len([a for a in last_14_days_actions if a.action == 'HELPFUL'])
+                doc.not_helpful_count = len([a for a in last_14_days_actions if a.action == 'NOT_HELPFUL'])
+                doc.comments = [a.data for a in last_14_days_actions if a.action == 'NOT_HELPFUL' and a.data]
+                doc.doc_raw_size = len(doc.comments) + 1
+                docs.append(doc)
+
+        context['docs'] = sorted(docs, key=lambda d: d.viewed_count, reverse=True)
+
+        return context
